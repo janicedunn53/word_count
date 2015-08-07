c53d1d07c2a3ecb7ae5837c304557786903654b5
Janice Dunn <janicedunn53@gmail.com>
2015-08-07 11:28:38 -0700

    Pass second spec that handles mixed letter casing in the phrase.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,5 +1,7 @@
 class String
   define_method(:word_count) do |word|
+    self.downcase!()
+    word.downcase!()
     phrase = []
     words = self.split()
     words.count(word)
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,8 @@ describe('String#word_count') do
   it("counts the number of a times a word occurs in a phrase") do
     expect(("peck of pickled peppers").word_count("peck")).to(eq(1))
   end
+
+  it("handles different letter casing in the phrase and in the word") do
+    expect(("Peck oF PicKled pePPers").word_count("pEck")).to(eq(1))
+  end
 end
