47ab2cc0232bc4fac143d45027550666565c842f
Janice Dunn <janicedunn53@gmail.com>
2015-08-07 11:18:58 -0700

    Pass first spec that counts number of times a word occurs in a phrase.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,14 +1,25 @@
 class String
-  define_method(:word_count) do
+  define_method(:word_count) do |word|
     phrase = []
-    word = self.split()
-    total = 0
-
-    phrase.each() do |item|
-      if item.eql?(word)
-      total = total.+(1)
-      end
-    end
-    total
+    words = self.split()
+    words.count(word)
   end
 end
+
+
+#class String
+#  define_method(:word_count) do |word|
+#    phrase = []
+#    word = self.split()
+#    total = 0
+#
+#    phrase.each() do |item|
+      #if item.eql?(word)
+      #if item == word
+      #if phrase.includes?(word)
+#      total = total.+(1)
+#      end
+#    end
+#    total
+#  end
+#end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,6 +3,6 @@ require('word_count')
 
 describe('String#word_count') do
   it("counts the number of a times a word occurs in a phrase") do
-    expect(("peck").word_count()).to(eq(2))
+    expect(("peck of pickled peppers").word_count("peck")).to(eq(1))
   end
 end
