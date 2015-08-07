db2a290ad051a6ec9d363ad872aec86091fce71d
Janice Dunn <janicedunn53@gmail.com>
2015-08-07 13:02:13 -0700

    Integration test passes so that it will return the number of times a word appears in a phrase.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/word_count')
+also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/count') do
+  @phrase = params.fetch('phrase')
+  @word = params.fetch('word')
+  @count = (@phrase).word_count(@word)
+  erb(:count)
 end
